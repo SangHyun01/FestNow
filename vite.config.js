5854e80b6ec283a3baf9a227d45fe8f501253256
Lee SangHyun <simon010809@gmail.com>
2025-03-27 21:04:32 +0900

    Initial setting

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // 자동 업데이트 설정
+      manifest: {
+        name: "페스나우",
+        short_name: "페스나우",
+        description: "축제 기간 다른 사람과 얘기해보세요",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
